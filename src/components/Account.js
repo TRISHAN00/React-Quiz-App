58dce3077c4524e01b648cec73f7ab3f8caf762d
TRISHAN00 <trishansaha43@gmail.com>
2022-03-27 22:30:25 +0600

    starting home page

--- src/components/Account.js
+++ src/components/Account.js
@@ -1,20 +1,18 @@
-import React from 'react';
+import React from "react";
+import classes from "../styles/Account.module.css";
 
 const Account = () => {
-    return (
-        <div>
-            <div className="account">
-          <span className="material-icons-outlined" title="Account">
-            account_circle
-          </span>
-          <a href="signup.html">Signup</a>
-          <span className="material-icons-outlined" title="Logout">
-            {" "}
-            logout{" "}
-          </span>
-        </div>
-        </div>
-    );
+  return (
+    <div>
+      <div className={classes.account}>
+        <span class="material-icons-outlined" title="Account">
+          account_circle
+        </span>
+        <a href="signup.html">Signup</a>
+        {/* <span class="material-icons-outlined" title="Logout"> logout </span> */}
+      </div>
+    </div>
+  );
 };
 
 export default Account;
--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,6 +1,5 @@
 import React from "react";
 import logo from '../assets/images/logo-bg.png';
-import '../styles/Nav.module.css';
 import classes from '../styles/Nav.module.css';
 import Account from "./Account";
 
@@ -8,16 +7,16 @@ const Nav = () => {
   return (
     <div>
       <nav className={classes.nav}>
-        <ul>
-          <li>
-            <a href="index.html" className={classes.brand}>
-              <img src={logo} alt="Learn with Sumit Logo" />
-              <h3>Learn with Sumit</h3>
-            </a>
-          </li>
-        </ul>
-        <Account/>
-      </nav>
+      <ul>
+        <li>
+          <a href="index.html" className={classes.brand}>
+            <img src={logo} alt="Learn with Sumit Logo" />
+            <h3>Learn with Sumit</h3>
+          </a>
+        </li>
+      </ul>
+      <Account/>
+    </nav>
     </div>
   );
 };
