27022a202264faac4e71f9e90c903f7d68074a86
TRISHAN00 <trishansaha43@gmail.com>
2022-04-10 12:34:24 +0600

    make two components called answers and progressbar

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,12 @@
-import '../styles/App.css';
-import Layout from './Layout';
-import Login from './Login';
+import "../styles/App.css";
+import Layout from "./Layout";
+import Quiz from "./pages/Quiz";
 
 const App = () => {
   return (
     <Layout>
-        <Login/>
+      <Quiz/>
+      
     </Layout>
   );
 };
