056e2719838d063e843e1de5d5b244b6eed61224
TRISHAN00 <trishansaha43@gmail.com>
2022-04-11 17:59:22 +0600

    made state into authContext file

--- src/contexts/AuthContext.js
+++ src/contexts/AuthContext.js
@@ -1,3 +1,8 @@
+import {
+    createUserWithEmailAndPassword,
+    getAuth,
+    updateProfile
+} from "firebase/auth";
 import React, { useContext, useState } from "react";
 import "../firebase";
 
@@ -9,11 +14,17 @@ export function useAuth() {
 
 export function AuthProvider({ children }) {
   const [loading, setLoading] = useState(true);
-  const [currentUser, setcurrentUser] = useState();
+  const [currentUser, setCurrentUser] = useState();
 
   //   signup function
-  function signup(email, password, username) {
-      
+  async function signup(email, password, username) {
+    const auth = getAuth();
+    await createUserWithEmailAndPassword(auth, email, password);
+
+    // update profile
+    await updateProfile(auth.currentUser, {
+      displayName: username,
+    });
   }
 
   return (
