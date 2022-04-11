90b0fbde336ce58e6a0d4cf94b92f2171d0cd86c
TRISHAN00 <trishansaha43@gmail.com>
2022-04-11 18:37:16 +0600

    split form component

--- src/components/pages/Signup.js
+++ src/components/pages/Signup.js
@@ -1,12 +1,7 @@
 import React from "react";
-import { Link } from "react-router-dom";
 import signupSvg from "../../assets/images/signup.svg";
-import classes from "../../styles/Signup.module.css";
-import Button from "../Button";
-import Checkbox from "../Checkbox";
-import Form from "../Form";
 import Illustration from "../Illustration";
-import TextInput from "../TextInput";
+import SignupForm from "../SignupForm";
 
 const Signup = () => {
   return (
@@ -14,27 +9,7 @@ const Signup = () => {
       <h1>Create an account</h1>
       <div className="column">
         <Illustration svg={signupSvg} />
-        <Form className={`${classes.signup} ${classes.form}`}>
-          <TextInput type="text" placeholder="Enter name" icon="person" />
-          <TextInput
-            type="text"
-            placeholder="Enter email"
-            icon="alternate_email"
-          />
-          <TextInput type="password" placeholder="Enter password" icon="lock" />
-          <TextInput
-            type="password"
-            placeholder="Confirm password"
-            icon="lock_clock"
-          />
-          <Checkbox text="I agree to the Terms &amp; Conditions" />
-          <Button>
-            <span>Submit now</span>
-          </Button>
-          <div className={classes.info}>
-              Already have an account? <Link to="/login">Login</Link> instead.
-            </div>
-        </Form>
+        <SignupForm/>
       </div>
     </div>
   );
