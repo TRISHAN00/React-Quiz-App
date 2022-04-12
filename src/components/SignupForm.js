b73f9e9326c4e7fc8c715d4786fa070b53d7a7c6
TRISHAN00 <trishansaha43@gmail.com>
2022-04-12 13:46:11 +0600

    modified of signup page

--- src/components/SignupForm.js
+++ src/components/SignupForm.js
@@ -1,33 +1,70 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-import Button from './Button';
-import Checkbox from './Checkbox';
-import Form from './Form';
-import TextInput from './TextInput';
+import { async } from "@firebase/util";
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+import { useAuth } from "../contexts/AuthContext";
+import Button from "./Button";
+import Checkbox from "./Checkbox";
+import Form from "./Form";
+import TextInput from "./TextInput";
 
 const SignupForm = () => {
+  const [username, setUsername] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
+  const [agree, setAgree] = useState("");
+  const [error, setError] = useState("");
+
+  const { signup } = useAuth();
+
+  async function handleSubmit(e) {
+    if(password !== confirmPassword) {
+      return setError("Password don't match.")
+    }
+  }
+
   return (
-    <Form className={{height: '500px'}}>
-          <TextInput type="text" placeholder="Enter name" icon="person" />
-          <TextInput
-            type="text"
-            placeholder="Enter email"
-            icon="alternate_email"
-          />
-          <TextInput type="password" placeholder="Enter password" icon="lock" />
-          <TextInput
-            type="password"
-            placeholder="Confirm password"
-            icon="lock_clock"
-          />
-          <Checkbox text="I agree to the Terms &amp; Conditions" />
-          <Button>
-            <span>Submit now</span>
-          </Button>
-          <div className="info">
-              Already have an account? <Link to="/login">Login</Link> instead.
-            </div>
-        </Form>
+    <Form className={{ height: "500px" }}>
+      <TextInput
+        type="text"
+        placeholder="Enter name"
+        icon="person"
+        value={username}
+        onChange={(e) => setUsername(e.target.value)}
+      />
+      <TextInput
+        type="text"
+        placeholder="Enter email"
+        icon="alternate_email"
+        value={email}
+        onChange={(e) => setEmail(e.target.value)}
+      />
+      <TextInput
+        type="password"
+        placeholder="Enter password"
+        icon="lock"
+        value={password}
+        onChange={(e) => setPassword(e.target.value)}
+      />
+      <TextInput
+        type="password"
+        placeholder="Confirm password"
+        icon="lock_clock"
+        value={confirmPassword}
+        onChange={(e) => setConfirmPassword(e.target.value)}
+      />
+      <Checkbox
+        text="I agree to the Terms &amp; Conditions"
+        value={agree}
+        onChange={(e) => setAgree(e.target.value)}
+      />
+      <Button>
+        <span>Submit now</span>
+      </Button>
+      <div className="info">
+        Already have an account? <Link to="/login">Login</Link> instead.
+      </div>
+    </Form>
   );
 };
 
