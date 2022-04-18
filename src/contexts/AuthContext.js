964bf5c37c6cd1949f1865374dbce2b3551aa2e5
TRISHAN00 <trishansaha43@gmail.com>
2022-04-18 13:58:47 +0600

    update signuup function

--- src/contexts/AuthContext.js
+++ src/contexts/AuthContext.js
@@ -1,4 +1,10 @@
+import {
+  createUserWithEmailAndPassword,
+  getAuth,
+  updateProfile
+} from "firebase/auth";
 import React, { useContext, useState } from "react";
+import "../firebase";
 
 const AuthContext = React.createContext();
 
@@ -10,6 +16,20 @@ export function useAuth() {
 export function AuthProvider({ children }) {
   const [loading, setLoading] = useState(true);
   const [currentUser, setCurrentUser] = useState();
+
+  // signup function
+  async function signup(email, password, username) {
+    const auth = getAuth();
+    await createUserWithEmailAndPassword(auth, email, password, username);
+
+    // update profile
+    updateProfile(auth.currentUser, {
+      displayName: username,
+    });
+
+    const user = auth.currentUser();
+    setCurrentUser({ ...user });
+  }
 
   return (
     <AuthContext.Provider value={value}>
