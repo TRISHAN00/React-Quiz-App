d41bed049e4d325dfab2ca3c90a027fc90fa1055
TRISHAN00 <trishansaha43@gmail.com>
2025-04-08 02:23:42 +0600

    not complte the navbar

---
+++ src/components/Brand.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+export default function Brand() {
+  return (
+    <ul>
+      <li>
+        <a href="index.html" class="brand">
+          <img src="./images/logo-bg.png" alt="Learn with Sumit Logo" />
+          <h3>Learn with Sumit</h3>
+        </a>
+      </li>
+    </ul>
+  );
+}
